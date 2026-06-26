d65a091468564ffd57d79be37132275322126e5b
Haven Developer <atedanielgreates@gmail.com>
2026-06-26 17:00:32 +0100

    Fix Authorization header in PHP instead of htaccess

--- public/config/auth.php
+++ public/config/auth.php
@@ -1,20 +1,25 @@
 <?php
 function get_authenticated_user() {
-    $headers = null;
-    if (function_exists('apache_request_headers')) {
+    $auth_header = null;
+    
+    if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+        $auth_header = $_SERVER['HTTP_AUTHORIZATION'];
+    } elseif (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+        $auth_header = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+    } elseif (function_exists('apache_request_headers')) {
         $headers = apache_request_headers();
+        $auth_header = isset($headers['Authorization']) ? $headers['Authorization'] : (isset($headers['authorization']) ? $headers['authorization'] : null);
     } else {
-        $headers = array();
         foreach($_SERVER as $key => $value) {
-            if (substr($key, 0, 5) <> 'HTTP_') {
-                continue;
+            if (substr($key, 0, 5) == 'HTTP_') {
+                $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+                if ($header == 'Authorization') {
+                    $auth_header = $value;
+                    break;
+                }
             }
-            $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-            $headers[$header] = $value;
         }
     }
-    
-    $auth_header = isset($headers['Authorization']) ? $headers['Authorization'] : (isset($headers['authorization']) ? $headers['authorization'] : null);
     
     if (!$auth_header || !preg_match('/Bearer\s(\S+)/', $auth_header, $matches)) {
         return null;
